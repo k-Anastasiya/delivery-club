6da107238f74917e48964fbb3b179f3769dc0fd1
Anastasiya Karpovich <nastasya.karpovich@gmail.com>
2022-06-27 21:20:53 +0300

    feat:task 2

--- src/test/java/ru/deliveryClub/FirstTest.java
+++ src/test/java/ru/deliveryClub/FirstTest.java
@@ -1,17 +1,38 @@
 package ru.deliveryClub;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
+    public ChromeDriver webdriver;
+    @Before
+    public void setUp(){
+    //    System.setProperty("webdriver.chrome.driver","C:\\IT\\webriver\\chromedriver.exe");
+        webdriver = new ChromeDriver();
+        System.out.println("test start");
+    }
     @Test
     public void firstTest() {
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.delivery-club.ru");
-        String title = webDriver.getTitle();
+        webdriver.get("https://www.delivery-club.ru");
+        String title = webdriver.getTitle();
         Assert.assertTrue(title.equals("Delivery Club — Доставка еды и продуктов"));
-        webDriver.quit();
+            }
+
+    @Test
+    public void secondTest() {
+        webdriver.get("https://www.delivery-club.ru");
+        String title = webdriver.getTitle();
+        Assert.assertTrue(title.equals("Delivery Club — Доставка еды и продуктов"));
+    }
+
+
+    @After
+    public void close(){
+        System.out.println("test close");
+        webdriver.quit();
     }
 }
